20ed86cb19eda501f417529a09c119add8e80508
Robin Malfait <malfait.robin@gmail.com>
2015-06-15 23:32:25 +0200

    Hmm, waiting with the database for now

--- src/Laravel/Commands/MakeEventStoreTableCommand.php
+++ src/Laravel/Commands/MakeEventStoreTableCommand.php
@@ -1,19 +1,7 @@
 <?php namespace EventSourcing\Laravel\Commands;
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
-
 class MakeEventStoreTableCommand extends EventSourcingCommand
 {
-
-    private $database;
-
-    function __construct(Builder $database)
-    {
-        $this->database = $database;
-    }
-
-
     /**
      *
      */
@@ -30,17 +18,17 @@ class MakeEventStoreTableCommand extends EventSourcingCommand
      */
     public function handle()
     {
-        $this->database->create('eventstore', function(Blueprint $table)
-        {
-            $table->string('uuid', 50)->index();
-
-            $table->integer('playhead')->unsigned();
-            $table->text('metadata');
-            $table->text('payload');
-            $table->dateTime('recorded_on');
-
-            $table->unique(['uuid', 'playhead']);
-        });
+//        $this->database->create('eventstore', function(Blueprint $table)
+//        {
+//            $table->string('uuid', 50)->index();
+//
+//            $table->integer('playhead')->unsigned();
+//            $table->text('metadata');
+//            $table->text('payload');
+//            $table->dateTime('recorded_on');
+//
+//            $table->unique(['uuid', 'playhead']);
+//        });
     }
 
 }
